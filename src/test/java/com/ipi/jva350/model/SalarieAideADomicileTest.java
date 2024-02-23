b2d133f6fa38f2a2c32ca441824d30cec3d965c1
Remi P <perez.remi@gmail.com>
2024-02-23 12:20:25 +0100

    suite résolution smell code 12h20

--- src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
+++ src/test/java/com/ipi/jva350/model/SalarieAideADomicileTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import java.time.LocalDate;
-import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class SalarieAideADomicileTest {
